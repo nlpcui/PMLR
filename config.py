67244e56d9cf46e23e2bbbce223525fb7079c999
nlpcui <penncui@foxmail.com>
2025-04-25 13:55:55 +0200

    update

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ local_config = {
     }
 }
 
-openai_key = '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'
+openai_key = ''
 
 api_price = {
     'gpt-4o-mini': {'input': 0.15 / 1000000, 'output': 0.06 / 1000000}
